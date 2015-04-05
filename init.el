1b6062af09459bc1596b0574d70a936fc16b3000
elefher <eldelielev@gmail.com>
2015-04-05 19:20:48 +0300

    change cedet to development cedet version due to a bug in emacs 24 version

--- init.el
+++ init.el
@@ -8,21 +8,33 @@
 ;(add-to-list 'package-archives
 ;             '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (package-initialize)
-
+(require 'cl)
 (add-to-list 'load-path "~/.emacs.d")
-;(load "arduino-mode.el")
+(load "arduino-mode.el")
 (require 'workgroups)
 
 (setq wg-prefix-key (kbd "C-c w"))
 (workgroups-mode 1)
-(wg-load "~/.emacs.d/")
 
-; setup cedet
-;(load-file "~/.emacs.d/cedet-1.1/common/cedet.el")
-;(global-ede-mode 1)                      ; Enable the Project management system
-;(semantic-load-enable-code-helpers)      ; Enable prototype help and smart completion 
-;(global-srecode-minor-mode 1)            ; Enable template insertion menu
+;; Load CEDET.
+;; See cedet/common/cedet.info for configuration details.
+;; IMPORTANT: Tou must place this *before* any CEDET component (including
+;; EIEIO) gets activated by another package (Gnus, auth-source, ...).
+(load-file ".emacs.d/cedet/cedet-devel-load.el")
+
+;; Add further minor-modes to be enabled by semantic-mode.
+;; See doc-string of `semantic-default-submodes' for other things
+;; you can use here.
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode t)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-completions-mode t)
+(add-to-list 'semantic-default-submodes 'global-cedet-m3-minor-mode t)
+
+;; Enable Semantic
+(semantic-mode 1)
+
+;; Enable EDE (Project Management) features
+(global-ede-mode 1)
 
 ; setup arduino mode
-;(setq auto-mode-alist (cons '("\\.\\(pde\\|ino\\)$" . arduino-mode) auto-mode-alist))
-;(autoload 'arduino-mode "arduino-mode" "Arduino editing mode." t)
+(setq auto-mode-alist (cons '("\\.\\(pde\\|ino\\)$" . arduino-mode) auto-mode-alist))
+(autoload 'arduino-mode "arduino-mode" "Arduino editing mode." t)
